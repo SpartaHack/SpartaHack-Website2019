3d984b7bb0a41e59bb97976829088ea7a9b519ce
HarrisonSanders <harrisonsanderslives@gmail.com>
2018-06-24 23:41:46 -0400

    routing fix

--- src/app/router.module.ts
+++ src/app/router.module.ts
@@ -1,3 +1,4 @@
+import { SignupFormComponent } from './signupform/signupform.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
@@ -7,7 +8,8 @@ import { FaqComponent } from './faq/faq.component';
 
 const routes: Routes = [
     { path: '', component: HomeComponent },
-    { path: 'faq', component: FaqComponent }
+    { path: 'faq', component: FaqComponent },
+    { path: 'register', component: SignupFormComponent}
 
 ];
 
