b3ff3ae41116a97294ce7e5f6be39a8dab701e2e
Jarek <jarektroyer@gmail.com>
2019-01-13 19:42:06 -0500

    branded icon implementation

--- src/app/shared/local-header/local-header.component.ts
+++ src/app/shared/local-header/local-header.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
     selector: 'local-header',
-    templateUrl: 'local-header.component.html'
+    templateUrl: 'local-header.component.html',
+    styleUrls: ['./local-header.component.scss']
 })
 
 export class LocalHeaderComponent implements OnInit {
